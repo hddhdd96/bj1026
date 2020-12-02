d523d6121e229e0495c6ae9f9fc1b2a37cb46f4d
hddhdd96 <ciko111@naver.com>
2020-12-02 16:22:53 +0900

    init

---
+++ bj1026.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+for x in range():
+    B[x]
+
+'''
+N = int(input())
+A = list(map(int, input("A의 배열을 입력하세요").split()))
+B = list(map(int, input("B의 배열을 입력하세요").split()))
+S = 0
+
+for i in range(N):
+  S += min(A)*max(B)
+  A.remove(min(A))
+  B.remove(max(B))
+
+print(S)
+'''
